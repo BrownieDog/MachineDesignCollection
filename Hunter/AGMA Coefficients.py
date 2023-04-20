dee7582c254bc3610779913d226866d8a9c9a1fa
ManOfStee1e <huntersteele13@gmail.com>
2023-04-20 08:46:02 -0600

    Making print statements for the AGMA coefficients

--- Hunter/AGMA Coefficients.py
+++ Hunter/AGMA Coefficients.py
@@ -19,50 +19,79 @@ def AGMA_coefficients(W_t, Q_v, V, P_d, N_cycle_P, N_cycle_G, F, p_x, pt_angle, 
 
 
     K_o = overload_factor()                         # overload factor
+    print(f"K_o = {K_o}")
+
     K_v = dynamic_factor(V, Q_v)                    # dynamic factor
+    print(f"K_v = {K_v}, where V = {V} and Q_v = {Q_v}")
+
     K_s = size_factor()                             # size factor
+    print(f"K_s = {K_s}")
+
     K_m = load_distribution_factor(d_P, F, S)       # load-distribution factor
+    print(f"K_m = {K_m}, where d_P = {d_P}, F = {F}, and S = {S}")
+
     K_B = rim_thickness_factor()                    # rim-thickness factor
+    print(f"K_B = {K_B}")
+
     J_P = bending_geometry_factor(p_x, F, N_P)      # J is geometry factor for bending stress including root fillet stress concentration factor - Fig 14-6 for the pinion
+    print(f"J_P = {J_P}, where p_x = {p_x}, F = {F}, and N_P = {N_P}")
+
     J_G = bending_geometry_factor(p_x, F, N_G)      # J is geometry factor for bending stress including root fillet stress concentration factor - Fig 14-6 for the gear
-
-
+    print(f"J_G = {J_G}, where p_x = {p_x}, F = {F}, and N_G = {N_G}")
 
     S_t = 45000     # guess                         # bending strength (lbf/in^2) - Table 14-3 or 14-4 and Fig 14-2, 14-3, and 14-4
+    print(f"S_t = {S_t}, which is a guess")
 
     Y_N_P = bending_stress_cycle_factor(N_cycle_P)  # Y_N is bending stress cycle life factor for the pinion
+    print(f"Y_N_P = {Y_N_P}, where N_cycle_P = {N_cycle_P}")
+
     Y_N_G = bending_stress_cycle_factor(N_cycle_G)  # Y_N is bending stress cycle life factor for the gear
+    print(f"Y_N_G = {Y_N_G}, where N_cycle_G = {N_cycle_G}")
 
     K_T = temperature_factor()                      # temperature factor
+    print(f"K_T = {K_T}")
+
     K_R = reliability_factor()                      # reliability factor
+    print(f"K_R = {K_R}")
 
     C_p = elastic_coefficient()                     # elastic coefficient (sqrt(lbf/in^2))
+    print(f"C_p = {C_p}")
+
     C_f = surface_condition_factor()                # surface condition factor
+    print(f"C_f = {C_f}")
+
     I = contact_geometry_factor(pt_angle, N_G, N_P, d_G, d_P, P_n)  # I is contact geometry factor for pitting resistance
-
-
+    print(f"I = {I}, where pt_angle = {pt_angle}, N_G = {N_G}, N_P = {N_P}, d_G = {d_G}, d_P = {d_P}, P_n = {P_n}")
 
     S_c = 170000    # guess                         # allowable contact stress (lbf/in^2) - Table 14-6, 14-7, and Fig 14-5
+    print(f"S_c = {S_c}, which is a guess")
 
     Z_N_P = contact_stress_cycle_factor(N_cycle_P)            # Z_N is wear/contact stress cycle life factor
+    print(f"Z_N_P = {Z_N_P}, where N_cycle_P = {N_cycle_P}")
+
     Z_N_G = contact_stress_cycle_factor(N_cycle_G)            # Z_N is wear/contact stress cycle life factor
+    print(f"Z_N_G = {Z_N_G}, where N_cycle_G = {N_cycle_G}")
 
     C_H_G = gear_hardness_ratio_factor(N_G, N_P, d_G, d_P)       # gear hardness ratio factors for pitting resistance
+    print(f"C_H_G = {C_H_G}, where N_G = {N_G}, N_P = {N_P}, d_G = {d_G}, d_P = {d_P}")
+
     C_H_P = pinion_hardness_ratio_factor()          # pinion hardness ratio factors for pitting resistance
+    print(f"C_H_P = {C_H_P}")
 
     # AGMA bending stress factor of safety, a stress ratio
     S_F_G = bending_safety_factor_AGMA(S_t, Y_N_G, K_T, K_R, W_t, K_o, K_v, K_s, P_d, F, K_m, K_B, J_G)     # gear bending factor of safety
-    S_F_P = bending_safety_factor_AGMA(S_t, Y_N_P, K_T, K_R, W_t, K_o, K_v, K_s, P_d, F, K_m, K_B, J_P)     # pinion bending factor of safety
+    print(f"S_F_G = {S_F_G}, where S_t = {S_t}, Y_N_G = {Y_N_G}, K_T = {K_T}, K_R = {K_R}, W_t = {W_t}, K_o = {K_o}, K_v = {K_v}, K_s = {K_s}, P_d = {P_d}, F = {F}, K_m = {K_m}, K_B = {K_B}, J_G = {J_G}")
 
+    S_F_P = bending_safety_factor_AGMA(S_t, Y_N_P, K_T, K_R, W_t, K_o, K_v, K_s, P_d, F, K_m, K_B, J_P)     # pinion bending factor of safety
+    print(f"S_F_P = {S_F_P}, where S_t = {S_t}, Y_N_P = {Y_N_P}, K_T = {K_T}, K_R = {K_R}, W_t = {W_t}, K_o = {K_o}, K_v = {K_v}, K_s = {K_s}, P_d = {P_d}, F = {F}, K_m = {K_m}, K_B = {K_B}, J_P = {J_P}")
 
     # AGMA wear/contact factor of safety, a stress ratio
     S_H_G = contact_safety_factor_AGMA(S_c, Z_N_G, C_H_G, K_T, K_R, C_p, W_t, K_o, K_v, K_s, K_m, d_P, F, C_f, I)
-    S_H_P = contact_safety_factor_AGMA(S_c, Z_N_P, C_H_P, K_T, K_R, C_p, W_t, K_o, K_v, K_s, K_m, d_P, F, C_f, I)
+    print(f"S_H_G = {S_H_G}, where S_c = {S_c}, Z_N_G = {Z_N_G}, C_H_G = {C_H_G}, K_T = {K_T}, K_R = {K_R}, C_p = {C_p}, W_t = {W_t}, K_o = {K_o}, K_v = {K_v}, K_s = {K_s}, K_m = {K_m}, d_P = {d_P}, F = {F}, C_f = {C_f}, and I = {I}")
 
-    print("Stress and I")
-    print(calc_contact_stress_AGMA(C_p, W_t, K_o, K_v, K_s, K_m, d_P, F, C_f, I))
-    print(I)
-    #print(Z_N)
+    S_H_P = contact_safety_factor_AGMA(S_c, Z_N_P, C_H_P, K_T, K_R, C_p, W_t, K_o, K_v, K_s, K_m, d_P, F, C_f, I)
+    print(f"S_H_G = {S_H_G}, where S_c = {S_c}, Z_N_G = {Z_N_P}, C_H_G = {C_H_P}, K_T = {K_T}, K_R = {K_R}, C_p = {C_p}, W_t = {W_t}, K_o = {K_o}, K_v = {K_v}, K_s = {K_s}, K_m = {K_m}, d_P = {d_P}, F = {F}, C_f = {C_f}, and I = {I}")
+
     return K_o, K_v, K_s, K_m, K_B, S_t, Y_N_P, Y_N_G, K_T, K_R, C_p, C_f, I, S_c, Z_N_P, Z_N_G, C_H_G, C_H_P, S_F_G, S_F_P, S_H_G, S_H_P
     # SCOTT, I replaced Y_N with Y_N_P and then added Y_N_G after that.
     # The new Y_N_P and Y_N_G take into account that the gear and pinion have different number of cycles.
